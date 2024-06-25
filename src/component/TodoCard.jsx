de940205a561c38b453e8569b4a096216fcfc823
MaheshwariButle07 <maheshwaributle07@gmail.com>
2024-06-25 21:46:49 +0530

    added changes to todo list

--- src/component/TodoCard.jsx
+++ src/component/TodoCard.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import "./TodoCard.css"
 
-function TodoCard({todoItems}) {
+function TodoCard({task,category}) {
+
+  const CATEGORY_EMOGI_MAP={
+    learning:"📚",
+    work:"👩🏻‍💻",
+    personal:"👼",
+    shopping:"🛍️",
+    health:"💪",
+    others:"➕"}
+
+  const CATEGORY_COLORS={
+    learning:"blue",
+    work:"green",
+    personal:"purple",
+    shopping:"orange",
+    health:"red",
+    others:"gray"
+  }  
+
   return (
-    <div>{todoItems}</div>
+    <div className='todo-card'>{task}  
+    <span className='todo-card-category'  style={{backgroundColor:CATEGORY_COLORS[category]}}> {CATEGORY_EMOGI_MAP[category]} {category}</span></div>
   )
 }
 
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Home.css"
 import TodoCard from '../component/TodoCard'
 
@@ -10,19 +10,39 @@ function Home() {
 
     const [newTask, setNewTask] = useState("")
 
+    const [category, setCategory] = useState("")
+
+    useEffect(()=>{
+        const savedList = localStorage.getItem("todoList")
+
+        if(savedList){
+            setTodoList(JSON.parse(savedList))
+        }
+    },[])
+
+   useEffect(()=>{
+    if(todoList.length===0) return
+
+    localStorage.setItem("todoList",JSON.stringify(todoList))
+   },[todoList])
 
     return (
         <>
 
             <h1 className='heading'>Todo App</h1>
             <div className='todo-container'>
-                {
-                    (todoList.map((todoItems, i) => <TodoCard key={i} todoItems={todoItems} />)
-                    )
+
+                {(todoList.map((todoItems, i) => {
+
+                    const { task, category } = todoItems
+
+                    return <TodoCard key={i} task={task} category={category} />
+                }))
+
                 }
 
                 {
-                    todoList.length === 0 ?  <p>No task to show, Please add new task</p> : null
+                    todoList.length === 0 ? <p className='task'>No task to show, Please add new task</p> : null
                 }
 
             </div>
@@ -36,6 +56,38 @@ function Home() {
                     value={newTask}
                     onChange={(e => setNewTask(e.target.value))} />
 
+                <select
+                    className='category'
+                    value={category}
+                    onChange={(e => setCategory(e.target.value))}>
+
+                    <option value="">
+                        Category
+                    </option>
+
+                    <option value="shopping">
+                        Shopping
+                    </option>
+                    <option value="learning">
+                        Learning
+                    </option>
+                    <option value="health">
+                        Health
+                    </option>
+                    <option value="personal">
+                        Personal
+                    </option>
+                    <option value="work">
+                        Work
+                    </option>
+                    <option value="others">
+                        Others
+                    </option>
+
+
+
+                </select>
+
 
                 <img
                     src="https://cdn-icons-png.flaticon.com/128/10358/10358033.png"
@@ -43,18 +95,25 @@ function Home() {
                     className='increment-btn'
                     onClick={() => {
 
-                        if(newTask === ""){
+                        if (newTask === "") {
                             toast.error("Task Cannot be empty...")
                             return
                         }
 
-                        setTodoList([...todoList, newTask])
+                        if (category === "") {
+                            toast.error("Category Cannot be empty...")
+                            return
+
+                        }
+
+                        setTodoList([...todoList, { task: newTask, category: category }])
                         setNewTask("")
+                        setCategory("")
                         toast.success("Task Added Successfully ...")
                     }} />
 
             </div>
-            <Toaster/>
+            <Toaster />
         </>
     )
 }
