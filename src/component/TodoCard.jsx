b643f1322e47dd144f3e4a735ba36d2d8f642dec
MaheshwariButle07 <maheshwaributle07@gmail.com>
2024-06-26 20:41:16 +0530

    added delete button

--- src/component/TodoCard.jsx
+++ src/component/TodoCard.jsx
@@ -1,28 +1,42 @@
 import React from 'react'
 import "./TodoCard.css"
+import DelImg from "./trash.png"
 
-function TodoCard({task,category}) {
+function TodoCard({index, task, category, deleteItem }) {
 
-  const CATEGORY_EMOGI_MAP={
-    learning:"📚",
-    work:"👩🏻‍💻",
-    personal:"👼",
-    shopping:"🛍️",
-    health:"💪",
-    others:"➕"}
+  const CATEGORY_EMOGI_MAP = {
+    learning: "📚",
+    work: "👩🏻‍💻",
+    personal: "👼",
+    shopping: "🛍️",
+    health: "💪",
+    others: "➕"
+  }
 
-  const CATEGORY_COLORS={
-    learning:"blue",
-    work:"green",
-    personal:"purple",
-    shopping:"orange",
-    health:"red",
-    others:"gray"
-  }  
+  const CATEGORY_COLORS = {
+    learning: "blue",
+    work: "green",
+    personal: "purple",
+    shopping: "orange",
+    health: "red",
+    others: "gray"
+  }
 
   return (
-    <div className='todo-card'> <span className='task'>{task}</span> 
-    <span className='todo-card-category'  style={{backgroundColor:CATEGORY_COLORS[category]}}> {CATEGORY_EMOGI_MAP[category]} {category}</span></div>
+    <div className='todo-card'>
+      <img src={DelImg} alt='delete-Image' className='delete' onClick={()=>{deleteItem(index)}}/>
+
+
+      <span className='task'>{task}</span>
+
+
+      <span
+        className='todo-card-category'
+        style={{ backgroundColor: CATEGORY_COLORS[category] }}>
+        {CATEGORY_EMOGI_MAP[category]}
+        {category}
+      </span>
+    </div>
   )
 }
 
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import "./Home.css"
 import TodoCard from '../component/TodoCard'
-
+import Swal from 'sweetalert2'
 import toast, { Toaster } from 'react-hot-toast'
 
 function Home() {
@@ -26,6 +26,44 @@ function Home() {
         localStorage.setItem("todoList", JSON.stringify(todoList))
     }, [todoList])
 
+    function deleteItem(index) {
+
+        Swal.fire({
+            title: "Are You Sure ?",
+            text: "You Want To Delete This Item ?",
+            icon: "warning",
+            showCancelButton: true,
+        }).then((result) => {
+
+            if (!result.isConfirmed) {
+               return
+            }
+
+            else{
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                  });
+            }
+
+            const newList = todoList.filter((item, i) => {
+                if (i != index) {
+                    return true
+                }
+    
+                else {
+                    return false
+                }
+            })
+            setTodoList(newList)
+
+            
+        })
+
+        
+    }
+
     return (
         <>
 
@@ -33,81 +71,81 @@ function Home() {
 
             <div className='todo-container'>
 
-            <div className='input-container'>
+                <div className='input-container'>
 
-                <input
-                    placeholder="Write Your List Item's Here" className='input-box'
-                    value={newTask}
-                    onChange={(e => setNewTask(e.target.value))} />
+                    <input
+                        placeholder="Write Your List Item's Here" className='input-box'
+                        value={newTask}
+                        onChange={(e => setNewTask(e.target.value))} />
 
-                <select
-                    className='category'
-                    value={category}
-                    onChange={(e => setCategory(e.target.value))}>
+                    <select
+                        className='category'
+                        value={category}
+                        onChange={(e => setCategory(e.target.value))}>
 
-                    <option value="">
-                        Category
-                    </option>
+                        <option value="">
+                            Category
+                        </option>
 
-                    <option value="shopping">
-                        Shopping
-                    </option>
-                    <option value="learning">
-                        Learning
-                    </option>
-                    <option value="health">
-                        Health
-                    </option>
-                    <option value="personal">
-                        Personal
-                    </option>
-                    <option value="work">
-                        Work
-                    </option>
-                    <option value="others">
-                        Others
-                    </option>
-
-
-
-                </select>
+                        <option value="shopping">
+                            Shopping
+                        </option>
+                        <option value="learning">
+                            Learning
+                        </option>
+                        <option value="health">
+                            Health
+                        </option>
+                        <option value="personal">
+                            Personal
+                        </option>
+                        <option value="work">
+                            Work
+                        </option>
+                        <option value="others">
+                            Others
+                        </option>
 
 
-                <img
-                    src="https://cdn-icons-png.flaticon.com/128/10358/10358033.png"
-                    alt='add-button'
-                    className='increment-btn'
-                    onClick={() => {
 
-                        if (newTask === "") {
-                            toast.error("Task Cannot be empty...")
-                            return
-                        }
-
-                        if (category === "") {
-                            toast.error("Category Cannot be empty...")
-                            return
-
-                        }
-
-                        setTodoList([...todoList, { task: newTask, category: category }])
-                        setNewTask("")
-                        setCategory("")
-                        toast.success("Task Added Successfully ...")
-                    }} />
-
-            </div>
-
-            <hr /><br/>
+                    </select>
 
 
-           
+                    <img
+                        src="https://cdn-icons-png.flaticon.com/128/10358/10358033.png"
+                        alt='add-button'
+                        className='increment-btn'
+                        onClick={() => {
+
+                            if (newTask === "") {
+                                toast.error("Task Cannot be empty...")
+                                return
+                            }
+
+                            if (category === "") {
+                                toast.error("Category Cannot be empty...")
+                                return
+
+                            }
+
+                            setTodoList([...todoList, { task: newTask, category: category }])
+                            setNewTask("")
+                            setCategory("")
+                            toast.success("Task Added Successfully ...")
+                        }} />
+
+                </div>
+
+                <hr /><br />
+
+
+
 
                 {(todoList.map((todoItems, i) => {
 
                     const { task, category } = todoItems
 
-                    return <TodoCard key={i} task={task} category={category} />
+                    return <TodoCard key={i} index={i} task={task} category={category} deleteItem={deleteItem} />
                 }))
 
                 }
